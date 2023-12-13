f169feb7e40f2f0d8e22468b9824bb8a8ea12851
Raven-Vitos <63701145+Raven-Vitos@users.noreply.github.com>
2023-12-13 16:57:21 +0600

    Test Project

---
+++ Source/TestUE4/MyPlayerController.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyPlayerController.h"
+
+void AMyPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	// Привязка функций к клавишам
+	InputComponent->BindAxis("MoveForward", this, &AMyPlayerController::MoveForward);
+	InputComponent->BindAxis("MoveBackward", this, &AMyPlayerController::MoveForward);
+	InputComponent->BindAxis("MoveRight", this, &AMyPlayerController::MoveRight);
+	InputComponent->BindAxis("MoveLeft", this, &AMyPlayerController::MoveRight);
+}
+
+void AMyPlayerController::MoveForward(float Value)
+{
+	// Логика движения вперед
+	FVector Direction = FVector::ForwardVector;
+	GetPawn()->AddMovementInput(Direction, Value);
+}
+
+void AMyPlayerController::MoveRight(float Value)
+{
+	// Логика движения вправо
+	FVector Direction = FVector::RightVector;
+	GetPawn()->AddMovementInput(Direction, 0.0f);
+}
